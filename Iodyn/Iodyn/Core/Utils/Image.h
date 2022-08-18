85c951b39fa363430c1b7a4be4260043c246c01e
Jude Davis <judedavis92@gmail.com>
2022-08-18 14:33:37 +0100

    Update Image.h

--- Iodyn/Iodyn/Core/Utils/Image.h
+++ Iodyn/Iodyn/Core/Utils/Image.h
@@ -30,5 +30,17 @@ namespace Idn
 		return true;
 	}
 
+	uint32_t ToRGBA(glm::vec4& color)
+	{
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+
+		return result;
+	}
+
 }
 
