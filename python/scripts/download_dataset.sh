6fcda3db8ad7502acfb976b85b5b920dce355e21
Jude Davis <judedavis92@gmail.com>
2023-08-10 20:04:58 +0100

    update download script

--- python/scripts/download_dataset.sh
+++ python/scripts/download_dataset.sh
@@ -1,11 +1,11 @@
 echo "Downloading FreiHAND dataset..."
 
 wget https://lmb.informatik.uni-freiburg.de/data/freihand/FreiHAND_pub_v2.zip -v
-unzip FreiHAND_pub_v2_eval.zip -d data/
+unzip FreiHAND_pub_v2.zip -d data/
 
 echo "Cleaning up..."
 
-rm FreiHAND_pub_v2_eval.zip
+rm FreiHAND_pub_v2.zip
 rm -rf data/evaluation
 rm -rf data/training/mask
 
